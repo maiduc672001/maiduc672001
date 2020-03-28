68c90e86341b3fc04aae078fdcd7a66faae59f59
ducmai672001 <nguyenduc372001@gmail.com>
2020-03-29 01:21:17 +0700

    create_question

---
+++ toeic-web-logic/src/main/java/vn/myclass/controller/web/ExerciseController.java
@@ -0,0 +1,18 @@
+package vn.myclass.controller.web;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/bai-tap-nghe.html")
+public class ExerciseController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd=req.getRequestDispatcher("views/web/exercise/detail.jsp");
+        rd.forward(req,resp);
+    }
+}
